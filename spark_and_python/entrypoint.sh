276e61a45ebece1b28ae754a6549b63ef1ddeb3d
dmitry <dmitry>
2018-03-09 19:39:57 +0100

    spark-base is ready

--- spark_and_python/entrypoint.sh
+++ spark_and_python/entrypoint.sh
@@ -2,11 +2,13 @@
 set -e
 
 if [ "$1" == "master" ]; then
-    #exec "${SPARK_HOME}/bin/spark-class" "org.apache.spark.deploy.master.Master"
-    exec "${SPARK_HOME}/sbin/start-master.sh"
+    exec "${SPARK_HOME}/bin/spark-class" "org.apache.spark.deploy.master.Master"
+    #exec "${SPARK_HOME}/sbin/start-master.sh"
     exec "jupyter notebook --ip=127.0.0.1 --allow-root --NotebookApp.password=sha1:966032cae12c:f109b896f13058b2f110a89d345114fb5a1080f6"
 elif [ "$1" == "slave" ]; then
     exec "${SPARK_HOME}/bin/spark-class" "org.apache.spark.deploy.worker.Worker" "${2}"
+elif [ "$1" == "bash" ]; then
+    exec "bash"
 else
     echo "Syntax: master or slave MASTER_URL"
     exit 1
