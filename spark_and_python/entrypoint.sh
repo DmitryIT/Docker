69aa8ebe6ac7b012a74a1d30e158bdb1e20a802d
dmitry <dmitry>
2018-03-17 20:47:19 +0100

    spark-base is ready

--- spark_and_python/entrypoint.sh
+++ spark_and_python/entrypoint.sh
@@ -2,9 +2,11 @@
 set -e
 
 if [ "$1" == "master" ]; then
-    exec "${SPARK_HOME}/bin/spark-class" "org.apache.spark.deploy.master.Master"
     #exec "${SPARK_HOME}/sbin/start-master.sh"
-    exec "jupyter notebook --ip=127.0.0.1 --allow-root --NotebookApp.password=sha1:966032cae12c:f109b896f13058b2f110a89d345114fb5a1080f6"
+    #exec "${SPARK_HOME}/bin/spark-class" "org.apache.spark.deploy.master.Master"
+    #exec "jupyter" "notebook" "--ip='*'" "--allow-root" "--NotebookApp.password=sha1:966032cae12c:f109b896f13058b2f110a89d345114fb5a1080f6"
+    $SPARK_HOME/sbin/start-master.sh
+    jupyter notebook --ip='*' --allow-root --NotebookApp.password=sha1:966032cae12c:f109b896f13058b2f110a89d345114fb5a1080f6
 elif [ "$1" == "slave" ]; then
     exec "${SPARK_HOME}/bin/spark-class" "org.apache.spark.deploy.worker.Worker" "${2}"
 elif [ "$1" == "bash" ]; then
